2089faf428fa1048e3ad372017d10af4d0ee43b7
Michael C. Grant <mgrant@anaconda.com>
2019-11-22 08:29:55 -0600

    More simplification

--- nb_conda_kernels/runner.py
+++ nb_conda_kernels/runner.py
@@ -3,21 +3,25 @@ from __future__ import print_function
 import os
 import sys
 import subprocess
+try:
+    from shlex import quote
+except ImportError:
+    from pipes import quote
 
 
 def exec_in_env(conda_root, envname, *command):
     # Run the standard conda activation script, and print the
     # resulting environment variables to stdout for reading.
-    command = subprocess.list2cmdline(command)
     if sys.platform.startswith('win'):
+        command = subprocess.list2cmdline(command)
         activate = os.path.join(conda_root, 'Scripts', 'activate.bat')
         activator = subprocess.list2cmdline(['call', activate, envname])
         ecomm = '{} & {}'.format(activator, command)
         subprocess.Popen(ecomm, shell=True).wait()
     else:
+        command = ' '.join(quote(c) for c in command)
         activate = os.path.join(conda_root, 'bin', 'activate')
-        activator = subprocess.list2cmdline(['.', activate, envname])
-        ecomm = '{} && exec {}'.format(activator, command)
+        ecomm = ". '{}' '{}' && exec {}".format(activate, envname, command)
         ecomm = ['sh' if 'bsd' in sys.platform else 'bash', '-c', ecomm]
         os.execvp(ecomm[0], ecomm)
 
